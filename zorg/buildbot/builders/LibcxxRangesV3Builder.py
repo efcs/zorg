927da0a0060c60b965dcf3727665eb4d687d8a84
Eric Fiselier <eric@efcs.ca>
2016-06-02 00:58:21 -0600

    changes

--- zorg/buildbot/builders/LibcxxRangesV3Builder.py
+++ zorg/buildbot/builders/LibcxxRangesV3Builder.py
@@ -111,7 +111,7 @@ def getLibcxxRangesV3Builder(f=None, env={}):
 
     # Configure Ranges
     libcxx_compile_args = properties.WithProperties(
-        '-nostdinc++ -cxx-isystem %(builddir)s/llvm/projects/libcxx/include/')
+        '-stdlib=libc++ -cxx-isystem %(builddir)s/llvm/projects/libcxx/include/')
     libcxx_link_args = properties.WithProperties(
         '-L%(builddir)s/build/lib/ -Wl,-rpath,%(builddir)s/build/lib/')
     env_cp = dict(env)
