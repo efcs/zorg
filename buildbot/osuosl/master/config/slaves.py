dcab582d3047df384a6d6dca4b66c54d7fab56b1
Eric Fiselier <eric@efcs.ca>
2016-06-14 04:01:49 -0600

    stuff

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -1,12 +1,12 @@
 import buildbot
-import buildbot.buildslave
+from buildbot.plugins import buildslave
 import os
 
 import config
 
 def create_slave(name, *args, **kwargs):
     password = config.options.get('Slave Passwords', name)
-    return buildbot.buildslave.BuildSlave(name, password=password, *args, **kwargs)
+    return buildslave.BuildSlave(name, password=password, *args, **kwargs)
 
 def get_build_slaves():
     return [
