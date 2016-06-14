c087d187d9023030e8ee3f7c58d3fccca3d887df
Eric Fiselier <eric@efcs.ca>
2016-06-14 05:19:40 -0600

    stuff

--- buildbot/osuosl/master/config/status.py
+++ buildbot/osuosl/master/config/status.py
@@ -1,15 +1,21 @@
 import os
 import buildbot
+import buildbot.status.html
+import buildbot.status.mail
+import buildbot.status.words
 
 import config
+from zorg.buildbot.util.ConfigEmailLookup import ConfigEmailLookup
+from zorg.buildbot.util.InformativeMailNotifier import InformativeMailNotifier
+
 # Returns a list of Status Targets. The results of each build will be
 # pushed to these targets. buildbot/status/*.py has a variety to choose from,
 # including web pages, email senders, and IRC bots.
 
 def get_status_targets(standard_builders, standard_categories=None):
 
+    from buildbot.status import html
     from buildbot.status.web import auth, authz
-    import buildbot.status.html
     authz_cfg=authz.Authz(
                       # change any of these to True to enable; see the manual for more
                       # options
@@ -22,7 +28,8 @@ def get_status_targets(standard_builders, standard_categories=None):
                       cancelPendingBuild = True,
                       )
 
-    return [
-         buildbot.status.html.WebStatus(http_port = 8080, authz=authz_cfg),
+    default_email = config.options.get('Master Options', 'default_email')
 
+    return [
+        buildbot.status.html.WebStatus(http_port = 8080, authz=authz_cfg),
     ]
