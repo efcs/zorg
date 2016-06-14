8a802974309c7faf703259282ecaa9a95cf598ab
Eric Fiselier <eric@efcs.ca>
2016-06-14 05:41:30 -0600

    stuff

--- buildbot/osuosl/master/config/status.py
+++ buildbot/osuosl/master/config/status.py
@@ -16,16 +16,13 @@ def get_status_targets(standard_builders, standard_categories=None):
 
     from buildbot.status import html
     from buildbot.status.web import auth, authz
-    authz_cfg=authz.Authz(
+    authz_cfg=auth.Authz(
                       # change any of these to True to enable; see the manual for more
                       # options
-                      gracefulShutdown   = False,
+
                       forceBuild         = True, # use this to test your slave once it is set up
-                      forceAllBuilds     = False,
-                      pingBuilder        = True,
                       stopBuild          = True,
-                      stopAllBuilds      = False,
-                      cancelPendingBuild = True,
+                      cancelPendingBuild = True
                       )
 
     default_email = config.options.get('Master Options', 'default_email')
