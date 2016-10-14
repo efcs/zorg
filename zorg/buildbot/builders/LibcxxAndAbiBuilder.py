7d1058be16ce469202bf876fc6091318157c3302
Eric Fiselier <eric@efcs.ca>
2016-10-14 04:59:13 -0600

    a

--- zorg/buildbot/builders/LibcxxAndAbiBuilder.py
+++ zorg/buildbot/builders/LibcxxAndAbiBuilder.py
@@ -64,7 +64,7 @@ def addTestSuite(litDesc, env={}):
     # Specify the max number of threads using properties so LIT doesn't use
     # all the threads on the system.
     litCmd = ['%(builddir)s/llvm/utils/lit/lit.py',
-              '-sv', '--show-unsupported', '--show-xfail', '--threads=%(jobs)s',
+              '-sv', '--show-unsupported', '--show-xfail',  '--threads=12', # FIXME '--threads=%(jobs)s',
               '--param=libcxx_site_config=%(builddir)s/build/projects/libcxx/test/lit.site.cfg']
 
     for key in litDesc.opts:
