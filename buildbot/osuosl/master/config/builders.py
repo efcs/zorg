a5ce124f1ef9baf1ebb894141308e31b2e2b2862
Eric Fiselier <eric@efcs.ca>
2017-11-18 19:13:48 -0700

    try change

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -86,7 +86,7 @@ def get_builders():
                 'LIBCXX_GENERATE_COVERAGE': 'ON',
                 'LIBCXX_COVERAGE_LIBRARY': '/usr/local/lib/clang/6.0.0/lib/linux/libclang_rt.profile-x86_64.a'},
             lit_invocations=default_args(),
-            generate_coverage='/var/www/html/efcs/efcs/libcxx-coverage'),
+            generate_coverage='/opt/libcxx-coverage'),
 
         getLibcxxRangesBuilder('ranges-v3'),
         getLibcxxBoostBuilder('boost')
