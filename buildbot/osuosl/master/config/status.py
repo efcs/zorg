88b7fc760e02dd522a0d6d13abed7d66d10fa424
Eric Fiselier <eric@efcs.ca>
2017-11-18 18:31:38 -0700

    try local

--- buildbot/osuosl/master/config/status.py
+++
@@ -1,5 +0,0 @@
-
-def get_status_targets(standard_builders, standard_categories=None):
-    return dict(port=8010,
-                plugins=dict(waterfall_view={},
-                console_view={}))
