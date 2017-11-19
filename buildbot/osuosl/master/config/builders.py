e7bb5661f3472c70c34fd2ede59d006aec338036
Eric Fiselier <eric@efcs.ca>
2017-11-18 19:08:11 -0700

    try change

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -84,7 +84,7 @@ def get_builders():
             cmake_opts={
                 'CMAKE_BUILD_TYPE': 'DEBUG',
                 'LIBCXX_GENERATE_COVERAGE': 'ON',
-                'LIBCXX_COVERAGE_LIBRARY': '/usr/local/lib/clang/4.0.0/lib/linux/libclang_rt.profile-x86_64.a'},
+                'LIBCXX_COVERAGE_LIBRARY': '/usr/local/lib/clang/6.0.0/lib/linux/libclang_rt.profile-x86_64.a'},
             lit_invocations=default_args(),
             generate_coverage='/var/www/html/efcs/efcs/libcxx-coverage'),
 
